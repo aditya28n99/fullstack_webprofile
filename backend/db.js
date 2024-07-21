09a15e89f6a081b228289294b072f0be6bd00712
Aditya Sunil Wakale <adityawakale208@gmail.com>
2024-07-21 20:51:33 +0530

    SSl and certification added

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,17 @@ require('dotenv').config();
 
 const mysql = require('mysql2');
 
+const fs = require('fs');
+const dotenv = require('dotenv');
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+  ca: fs.readFileSync(process.env.CA)
+  }
 });
 
 db.connect(err => {
